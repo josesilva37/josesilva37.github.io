eb7baaf41b5608496df717e3f9dbdc404b64d001
josesilva37 <josesilva.jpas@gmail.com>
2021-04-13 10:33:37 +0100

    t

--- Ficha8/index.js
+++ Ficha8/index.js
@@ -1,12 +1,73 @@
-var conta= document.getElementById("conta");
-var resultado = document.getElementById("resultado");
-var input = document.createElement("input");
-var res = document.createElement("res");
+var container = document.getElementById("container");
+var butoes = document.getElementsByClassName("button");
 
-res.type= "text"
-res.name="res_display"
-input.type="text";
-input.name="conta_input";
+for (let i = 0; i < butoes.length; i++) {
+    butoes[i].addEventListener("click", click);
+    butoes[i].addEventListener("mouseover", mouseOver);
+    butoes[i].addEventListener("mouseout", mouseOut);
+}
 
-conta.appendChild(input);
-resultado.appendChild(res);
+function click(butao) {
+    let input = document.getElementsByClassName("input")[0];
+    let resultado = document.getElementsByClassName("display")[0];
+    switch (butao.target.innerHTML) {
+        case "0":
+            input.innerHTML += "0";
+            break;
+        case "1":
+            input.innerHTML += "1";
+            break;
+        case "2":
+            input.innerHTML += "2";
+            break;
+        case "3":
+            input.innerHTML += "3";
+            break;
+        case "4":
+            input.innerHTML += "4";
+            break;
+        case "5":
+            input.innerHTML += "5";
+            break;
+        case "6":
+            input.innerHTML += "6";
+            break;
+        case "7":
+            input.innerHTML += "7";
+            break;
+        case "8":
+            input.innerHTML += "8";
+            break;
+        case "9":
+            input.innerHTML += "9";
+            break;
+        case "+":
+            input.innerHTML += "+";
+            break;
+        case "-":
+            input.innerHTML += "-";
+            break;
+        case "*":
+            input.innerHTML += "*";
+            break;
+        case "/":
+            input.innerHTML += "/";
+            break;
+        case "C":
+            input.innerHTML = "";
+            resultado.innerHTML= "";
+            break;
+        case "=":
+            resultado.innerHTML = eval(input.innerHTML);
+            break;
+
+    }
+}
+
+function mouseOut(butao) {
+    butao.target.style.backgroundColor = "green";
+}
+
+function mouseOver(butao) {
+    butao.target.style.backgroundColor = "grey";
+}

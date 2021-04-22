5916f1f72b652a55745dbb03f9404eb5bd44d506
josesilva37 <josesilva.jpas@gmail.com>
2021-04-22 09:09:19 +0100

    teste

--- Ficha10/index.js
+++ Ficha10/index.js
@@ -1,11 +1,56 @@
 let bAdd=document.getElementById("bAdd");
 
+instance = new dtsel.DTS('input[name="dateTimePicker"]');
+
 bAdd.addEventListener("click", adicionar);
 
 function adicionar(){
-    let input = document.getElementById("tarefa");;
-    let t = document.createElement("div");
-    t.className="tarefas";
-    t.innerHTML = input.innerHTML;
-    input.innerHTML="";
+    let input = document.getElementById("tarefa");
+    let data = document.getElementById("data");
+    let conTarefas = document.getElementsByClassName("tarefas")[0];
+    if(input.innerText.length > 0 && data.value.length >0){
+        let tc = document.createElement("div");
+        tc.className="lista";
+        conTarefas.appendChild(tc);
+        let t = document.createElement("div");
+        t.className = "lista";
+        t.innerText = input.innerText;
+        tc.appendChild(t);
+        let d = document.createElement("div");
+        d.className="lista";
+        d.innerText = data.value;
+        tc.appendChild(d);
+        let b = document.createElement("div");
+        b.innerText="-";
+        b.className="bRemove";
+        b.addEventListener("click", remover);
+        tc.appendChild(b);
+
+        tc.addEventListener("click",editar);
+
+        //Reset dados
+        input.innerHTML="";
+        data.value = "";
+    }
+    
+}
+
+function remover(e){
+    e.currentTarget.parentNode.remove();
+}
+
+function editar(e){
+    let tc = e.currentTarget;
+    let input = document.getElementById("tarefa");
+    let data = document.getElementById("data");
+    e.currentTarget.style.backgroundColor= "black";
+    let c = e.currentTarget.childNodes;
+    input.innerText = c[0].innerText;
+    data.value = c[1].innerText;
+    bAdd.innerText="v";
+    function remove(e){
+        tc.remove();
+        bAdd.innerText="+";
+    }
+    bAdd.addEventListener("click", remove);
 }

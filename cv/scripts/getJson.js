7abc729a06a20ca35b335ac79a9bd40ffabc5254
josesilva37 <josesilva.jpas@gmail.com>
2021-05-24 15:41:15 +0100

    cv

--- cv/scripts/getJson.js
+++ cv/scripts/getJson.js
@@ -6,8 +6,10 @@ var email = document.getElementById('email');
 var lingua = document.getElementById('lingua');
 var sobre = document.getElementById('sobre');
 //educacao
-var dataSecundario = document.getElementById('dataSecundario');
-var dataLicenciatura = document.getElementById('dataLicenciatura');
+
+var containerEducation = document.getElementById("container_education");
+
+
 
 async function getjson(){
     const response = await fetch("./cv.json");
@@ -26,5 +28,7 @@ function show(data){
     lingua.innerHTML = data.languages;
     sobre.innerText = data.basics.desc;
     //educacao
-
+    for(r of data.education){
+        containerEducation.insertAdjacentHTML('<div class="card"> <div class="row"><div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500"><div class="card-body cc-education-header"><p>' + r.startDate + '-' + r.endDate+ '</p><div class="h5"></div></div></div></div></div>')
+    }
 }

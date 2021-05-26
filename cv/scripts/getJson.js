4b9d6c2b0281431ecd1e6af8a73061910db57480
josesilva37 <josesilva.jpas@gmail.com>
2021-05-26 18:06:21 +0100

    cv

--- cv/scripts/getJson.js
+++ cv/scripts/getJson.js
@@ -6,9 +6,9 @@ var email = document.getElementById('email');
 var lingua = document.getElementById('lingua');
 var sobre = document.getElementById('sobre');
 //educacao
-
 var containerEducation = document.getElementById("container_education");
-
+//skills
+var listaSkills = document.getElementById="listaSkills";
 
 
 async function getjson(){
@@ -31,7 +31,10 @@ function show(data){
      //containerEducation.insertAdjacentHTML('<div class="card"> <div class="row"><div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500"><div class="card-body cc-education-header"><p></p><div class="h5"></div></div></div></div></div>');
 
     for(r of data.education){
-        containerEducation.insertAdjacentHTML('beforeend','<div class="card"> <div class="row"><div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500"><div class="card-body cc-education-header"><p>' + r.startDate + '-' + r.endDate+ '</p><p>' + r.area+ '</p><p> ' +  r.studyType + ' </p><div class="h5"></div></div></div><p>'+ "Estudou no instituto" + r.institution + "no curso de" + r.area+'</p></div></div>');
+        containerEducation.insertAdjacentHTML('beforeend','<div class="card"> <div class="row"><div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500"><div class="card-body cc-education-header"><p>' + r.startDate + '-' + r.endDate+ '</p><p>' + r.area+ '</p><p> ' +  r.studyType + ' </p><div class="h5"></div></div></div><p>'+ "Estudou no instituto " + r.institution + " no curso de " + r.area+'</p></div></div>');
     }
 
+    for(r of data.skills){
+        listaSkills.insertAdjacentHTML('beforeend','<ul>'+ r+ '</ul>')
+    }
 }

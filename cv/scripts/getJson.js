133f0fce00f671342dfe7b3cfe705ce9a3c3073b
josesilva37 <josesilva.jpas@gmail.com>
2021-05-28 18:50:52 +0100

    cv

--- cv/scripts/getJson.js
+++ cv/scripts/getJson.js
@@ -46,8 +46,12 @@ function show(data) {
     var lenght =Object.keys(data.projetos).length/2; 
     if(Object.keys(data.projetos).length > 1){
         for(var i=0;i<lenght;i+2){
-            containerPortofolio.insertAdjacentHTML('beforeend','<div class="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"> <a href="#web-development"> <figure class="cc-effect"><img src="'+ data.projetos[i].imgUrl +'" alt="Image" /> <figcaption> <div class="h4">'+ data.projetos[i].name+ '</div> <p>'+ data.projetos[i].desc+'</p> </figcaption> </figure> </a></div>');
-            containerPortofolio2.insertAdjacentHTML('beforeend','<div class="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"> <a href="#web-development"> <figure class="cc-effect"><img src="'+ data.projetos[i+1].imgUrl+'" alt="Image" /> <figcaption> <div class="h4">'+ data.projetos[i+1].name+ '</div> <p>'+ data.projetos[i+1].desc+'</p> </figcaption> </figure> </a></div>');
+            if(data.projetos[i] != null){
+                containerPortofolio.insertAdjacentHTML('beforeend','<div class="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"> <a href="#web-development"> <figure class="cc-effect"><img src="'+ data.projetos[i].imgUrl +'" alt="Image" /> <figcaption> <div class="h4">'+ data.projetos[i].name+ '</div> <p>'+ data.projetos[i].desc+'</p> </figcaption> </figure> </a></div>');
+            }
+            if(data.projetos[i+1] !=null){
+                containerPortofolio2.insertAdjacentHTML('beforeend','<div class="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"> <a href="#web-development"> <figure class="cc-effect"><img src="'+ data.projetos[i+1].imgUrl+'" alt="Image" /> <figcaption> <div class="h4">'+ data.projetos[i+1].name+ '</div> <p>'+ data.projetos[i+1].desc+'</p> </figcaption> </figure> </a></div>');
+            }
         }
     }else{
         for(p of data.projetos){
